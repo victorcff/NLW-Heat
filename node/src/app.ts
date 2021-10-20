c947af2aa96af2ba23ffac8a8b5dd0729bb15ba8
Victor Cavalcanti <victor.cavalcanti@sempreceub.com>
2021-10-20 17:15:06 -0300

    NLW Heat inicial

---
+++ node/src/app.ts
@@ -0,0 +1,38 @@
+import "dotenv/config"
+import express, { response } from "express";
+import { router } from "./routes";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors);
+
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on("connection", socket => {
+    console.log(`Usuário conectado no socket ${socket.id}`);
+});
+
+app.use(express.json());
+app.use(router);
+
+app.get('/github', (request, response) => {
+    response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
+});
+
+app.get('/signin/callback', (resquest, response) => {
+    const { code } = resquest.query;
+
+    return response.json(code);
+});
+
+
+
+export{ serverHttp, io };
